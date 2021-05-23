7dc552d28931a6a6cd17b4682b34969a6b7bf1cf
Ahd Ben Kheder <ahd.benkheder@gmail.com>
2021-05-23 11:35:12 +0200

    project cleanup

--- EmtyArray/EmtyArray/EmtyArray/EmtyArrayAnalyzer.cs
+++ EmtyArray/EmtyArray/EmtyArray/EmtyArrayAnalyzer.cs
@@ -31,24 +31,9 @@ namespace EmtyArray
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            // Your diagnostic registration here
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
-        }
     }
 }
